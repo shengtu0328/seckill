bdf895b2752f56ee1363a17ca2d3b8b0f1796ac8
rqxiao <1007689622@qq>
2018-12-06 16:32:22 +0800

    1

--- src/main/java/com/xrq/seckill/App.java
+++ src/main/java/com/xrq/seckill/App.java
@@ -1,7 +1,12 @@
 package com.xrq.seckill;
 
+import com.xrq.seckill.dao.UserDOMapper;
+import com.xrq.seckill.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.xrq.seckill"})
+@MapperScan("com.xrq.seckill.dao")
 @RestController
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+
+
     @RequestMapping("/")
     public String home(){
-        return "hello world";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO==null){
+            return "用户对象不存在";
+        }else {
+            return userDO.getName();
+        }
     }
 
 
